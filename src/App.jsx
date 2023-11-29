6feddf33a81fc51ff92ef7525e0c28c9c1ff8e32
XeroWTF <eugedsr@gmail.com>
2023-11-28 21:49:18 -0300

    Tablero Basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,71 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-return <h1>Tic Tac Toe</h1>
+const TURNS = {
+  X: "x",
+  O: "o"
+}
 
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+
+  const className = `square ${isSelected ? "is-selected" : ""}`
+
+  const handleClick = () => {
+    updateBoard();
+  }
+
+  return (
+    <div onClick={handleClick} className={className}  >
+      {children}
+    </div>
+  )
+}
+
+function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+  console.log(board);
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+      const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+  }
+
+  return (
+
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+
+              </Square>
+
+
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
 }
 
 export default App
+
+
